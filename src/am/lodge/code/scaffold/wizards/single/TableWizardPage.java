215a6be2b59810c9536129634e8055e42bdf4721
am <am@am-pc>
2013-07-12 18:46:15 +0800

    use Filtered Tree

--- src/am/lodge/code/scaffold/wizards/single/TableWizardPage.java
+++ src/am/lodge/code/scaffold/wizards/single/TableWizardPage.java
@@ -3,7 +3,15 @@ package am.lodge.code.scaffold.wizards.single;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TreeNode;
+import org.eclipse.jface.viewers.TreeNodeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -15,8 +23,9 @@ import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.dialogs.FilteredTree;
+import org.eclipse.ui.dialogs.PatternFilter;
 
 import am.lodge.code.scaffold.wizards.BaseNewWizard;
 
@@ -24,7 +33,7 @@ public class TableWizardPage extends WizardPage{
 
   private Button removePrefix;
 
-  private Tree tableTree;
+  private FilteredTree tableTree;
 
   private boolean enableNext;
 
@@ -57,22 +66,28 @@ public class TableWizardPage extends WizardPage{
     formData.top = new FormAttachment(0, 0);
     removePrefix.setLayoutData(formData);
 
-    tableTree = new Tree(body, SWT.SINGLE | SWT.BORDER);
+    tableTree = new FilteredTree(body, SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL | SWT.SINGLE, new PatternFilter(), true);
     formData = new FormData();
     formData.left = new FormAttachment(0, 0);
-    formData.top = new FormAttachment(label, 5);
+    formData.top = new FormAttachment(removePrefix, 5);
     formData.right = new FormAttachment(100, -1);
     formData.bottom = new FormAttachment(100, -1);
     tableTree.setLayoutData(formData);
-    tableTree.addSelectionListener(new SelectionAdapter(){
+    TreeViewer treeViewer = tableTree.getViewer();
+    treeViewer.setContentProvider(new TreeNodeContentProvider());
+    treeViewer.setLabelProvider(new TreeViewerLabelProvider());
+    treeViewer.setInput(new TreeNode[]{getTreeNode()});
+    treeViewer.getTree().addSelectionListener(new SelectionAdapter(){
       public void widgetSelected(SelectionEvent e){
         TreeItem item = (TreeItem)e.item;
-        Object v = item.getData("isTable");
+        TreeNode treeNode = (TreeNode)item.getData();
         boolean chahe = enableNext;
-        enableNext = v != null;
+        enableNext = treeNode.getValue() instanceof Map;
         if(enableNext){
-          selectTable = item.getText();
-          selectSchem = (String) item.getData("schem");
+          @SuppressWarnings("unchecked")
+          Map<String, String> date = (Map<String, String>) treeNode.getValue();
+          selectTable = date.get("tableName");
+          selectSchem = date.get("schem");
         }
         if(chahe != enableNext){
           canFlipToNextPage();
@@ -80,33 +95,54 @@ public class TableWizardPage extends WizardPage{
         }
       }
     });
-    initTableTree();
     setControl(body);
   }
 
-  private void initTableTree(){
+
+  class TreeViewerLabelProvider extends LabelProvider{
+    @SuppressWarnings("unchecked")
+    public String getText(Object obj) {
+      TreeNode node = (TreeNode)obj;
+      Object value = node.getValue();
+      if (value instanceof String) {
+        return (String)value;  
+      }else{
+        return ((Map<String, String>)value).get("tableName");
+      }
+    } 
+  }
+
+  private TreeNode getTreeNode(){
     DatabaseMetaData dmd = ((BaseNewWizard)getWizard()).getDatabaseMetaData();
+    TreeNode root = new TreeNode(null);
     try{
       String databaseName = dmd.getDatabaseProductName();
-      TreeItem root = new TreeItem(tableTree, SWT.NONE);
-      root.setText(databaseName);
+      root = new TreeNode(databaseName);
       ResultSet rs = dmd.getSchemas();
+      List<TreeNode> schemList = new ArrayList<TreeNode>();
       while (rs.next()){
-        TreeItem item = new TreeItem(root, SWT.NONE);
         String schem = rs.getString("TABLE_SCHEM");
-        item.setText(schem);
+        TreeNode schemNode = new TreeNode(schem);
         ResultSet tables = dmd.getTables(null, schem, "%", new String[] { "TABLE" });
+        schemNode.setParent(root);
+        schemList.add(schemNode);
+        List<TreeNode> tableList = new ArrayList<TreeNode>();
         while (tables.next()){
           String tableName = tables.getString("TABLE_NAME");
-          TreeItem sub = new TreeItem(item, SWT.NONE);
-          sub.setText(tableName);
-          sub.setData("schem", schem);
-          sub.setData("isTable", true);
+          Map<String, String> data = new HashMap<String, String>();
+          data.put("tableName", tableName);
+          data.put("schem", schem);
+          TreeNode tableNode = new TreeNode(data);
+          tableNode.setParent(schemNode);
+          tableList.add(tableNode);
         }
+        schemNode.setChildren((TreeNode[])tableList.toArray(new TreeNode[0]));
       }
+      root.setChildren((TreeNode[])schemList.toArray(new TreeNode[0]));
     }catch (SQLException e){
       throw new RuntimeException(e);
     }
+    return root;  
   }
 
   @Override
